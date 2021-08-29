bc3d6c9d13dadaa354bcb143331c983d95d3f047
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-08-29 22:06:12 +0530

    ReadMe Added

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Browse from './Browse';
+// EITHER  IMPORT A/Q to WEB PAGE YOU WANT TO SEE
+// import Browse from './Browse';
+// import Discover from './Discover';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Browse />
+  {/* EITHER TYPE BROWSE OR DISCOVER */}
+ 
   </React.StrictMode>,
   document.getElementById('root')
 );
